29e65c4f674df2a75a43bf28acc2012884198ce6
papepopa <ursachen_gallen.07@icloud.com>
2025-09-30 19:26:58 +0200

    ok it kinda work i cheaty :)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
-#for word in get
-print(
-    get_num_words("books/frankenstein.txt")
-    )
+dicktschneri = dict()
+for word in get_num_words("books/frankenstein.txt"):
+    for char in word:
+        dicktschneri[char] = dicktschneri.get(char, 0) + 1
+print(dicktschneri)

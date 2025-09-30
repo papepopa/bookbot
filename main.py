9aa56a71450825b8e22fecc093853969555c0c7c
papepopa <ursachen_gallen.07@icloud.com>
2025-09-30 20:09:31 +0200

    almmostndone

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from stats import get_num_words
-dicktschneri = dict()
+
+dicktschneri = {}
+
 for word in get_num_words("books/frankenstein.txt"):
+
     for char in word:
         dicktschneri[char] = dicktschneri.get(char, 0) + 1
-print(dicktschneri)
+
+list_of_dicks = list()
+
+def sorter(item):
+    return item["num"]
+
+for dick in dicktschneri:
+    if dick.isalpha():
+        num = dicktschneri[dick]
+        list_of_dicks.append({"char": dick, "num": num})
+
+list_of_dicks.sort(reverse=True, key=sorter)
+
+print(
+    "--------- Character Count -------"
+)
--- stats.py
+++ stats.py
@@ -1,9 +1,21 @@
 def get_num_words(path):
     with open(path) as f:
         string = f.read()
+        
     words_list = string.lower().split()
     num_words = len(words_list)
 
-    print(f"Found {num_words} total words")
+    print(
+        "============ BOOKBOT ============"
+    )
+    print(
+        f"Analyzing book found at {path}..."
+    )
+    print(
+        "----------- Word Count ----------"
+    )
+    print(
+        f"Found {num_words} total words"
+    )
 
     return words_list

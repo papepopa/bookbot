07cbfedeccd85a1e0bb8d8f691917fc3a75002b5
papepopa <ursachen_gallen.07@icloud.com>
2025-09-30 20:48:23 +0200

    it'sa done mamma mia

--- main.py
+++ main.py
@@ -1,24 +1,55 @@
 from stats import get_num_words
+import sys
 
-dicktschneri = {}
+def sorter_num(item):
+    return item["num"]
 
-for word in get_num_words("books/frankenstein.txt"):
+def sorter_char(item):
+    return item["char"]
+try:
+    path = sys.argv[1]
+except Exception:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+big_ass_string = get_num_words(path)
 
-    for char in word:
-        dicktschneri[char] = dicktschneri.get(char, 0) + 1
+num_words = len(big_ass_string)
 
 list_of_dicks = list()
 
-def sorter(item):
-    return item["num"]
+dicktschneri = {}
+
+for word in big_ass_string:
+    for char in word:
+        if char.isalpha():
+            dicktschneri[char] = dicktschneri.get(char, 0) + 1
 
 for dick in dicktschneri:
-    if dick.isalpha():
-        num = dicktschneri[dick]
-        list_of_dicks.append({"char": dick, "num": num})
+    num = dicktschneri[dick]
+    list_of_dicks.append({"char": dick, "num": num})
 
-list_of_dicks.sort(reverse=True, key=sorter)
+list_of_dicks.sort(reverse=True, key=sorter_num)
 
 print(
-    "--------- Character Count -------"
+        "============ BOOKBOT ============"
+)
+print(
+        f"Analyzing book found at {path}..."
+)
+print(
+        "----------- Word Count ----------"
+)
+print(
+        f"Found {num_words} total words"
+)
+
+print(
+        "--------- Character Count -------"
+)
+
+for alpha in list_of_dicks:
+    print(f"{sorter_char(alpha)}: {sorter_num(alpha)}")
+
+print(
+        "============= END ==============="
 )
--- stats.py
+++ stats.py
@@ -1,21 +1,6 @@
 def get_num_words(path):
     with open(path) as f:
         string = f.read()
-        
+
     words_list = string.lower().split()
-    num_words = len(words_list)
-
-    print(
-        "============ BOOKBOT ============"
-    )
-    print(
-        f"Analyzing book found at {path}..."
-    )
-    print(
-        "----------- Word Count ----------"
-    )
-    print(
-        f"Found {num_words} total words"
-    )
-
     return words_list
